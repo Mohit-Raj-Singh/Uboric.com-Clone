399fdac3ff7d197c2d8e9521b754dbfbb20d3b94
Mohit Raj Singh <mohit94e@gmail.com>
2022-11-10 22:06:56 +0530

    day3 complete

--- U-shopic/src/App.js
+++ U-shopic/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import AllRoutes from "./Pages/AllRoutes";
 import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      {/* <AllRoutes /> */}
+
+
       <Footer />
     </div>
   );
